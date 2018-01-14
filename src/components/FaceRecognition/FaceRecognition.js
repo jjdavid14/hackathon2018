8c0b0ba05d0b641a78ee1331a7db26b0ba1991d9
Kia Choi <sgchoi529@email.arizona.edu>
2018-01-14 05:37:03 -0700

    fix

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,21 +1,6 @@
 import React from 'react';
 
 const FaceRecognition = ({ imageUrl }) => {
-<<<<<<< HEAD
-    return (
-        <div className='center ma'>
-            <div className='absolue mt2'>
-                <img
-                    id='inputimage'
-                    alt=''
-                    src={imageUrl}
-                    width='500px'
-                    height='auto'
-                />
-            </div>
-        </div>
-    );
-=======
 	return (
 		<div className='center ma'>
 			<div className='absolute mt2'>
@@ -23,7 +8,6 @@ const FaceRecognition = ({ imageUrl }) => {
 			</div>
 		</div>
 		);
->>>>>>> 08c11f953e9fb06e946aa0d8ce849a7e288b6989
 }
 
 export default FaceRecognition;
